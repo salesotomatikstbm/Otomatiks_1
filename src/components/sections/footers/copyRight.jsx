e868bd8999a058ee2548830a8bc5278ba9688931
arungovindhasamygithub <arungwork63@gmail.com>
2025-02-02 14:25:43 +0530

    footer

--- src/components/sections/footers/copyRight.jsx
+++ src/components/sections/footers/copyRight.jsx
@@ -9,7 +9,7 @@ const CopyRight = ({ color }) => {
         <div className="pt-[75px] overflow-x-hidden">
             <div className="flex lg:flex-row flex-col justify-between lg:items-center pt-7.5 pb-8 border-t border-t-white border-opacity-20">
                 <p className={cn('wow fadeInLeft', color)} data-wow-delay=".3s">
-                    © <Link to="#">Otomatiks</Link> {currentYear} | All Rights Reserved
+                    © <Link to="#">Otomatiks</Link> {currentYear} | All Rights Reserved. Designed by Otomatiks
                 </p>
                 <ul className="flex items-center gap-7.5 wow fadeInRight" data-wow-delay=".3s">
                     {/* <li><Link to="#" className={cn(``, color)}>Terms & Condition</Link></li>
