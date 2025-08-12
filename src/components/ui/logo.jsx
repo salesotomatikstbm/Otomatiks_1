b44e4de34c68cedbcee0947e0f012a3c23165895
arungovindhasamygithub <arungwork63@gmail.com>
2025-08-12 13:50:15 +0530

    mobile view logo update

--- src/components/ui/logo.jsx
+++ src/components/ui/logo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from "@/assets/images/logo.svg" // Ensure path is correct
+import logo from "@/assets/images/logo.svg"     // Desktop logo
+import logomobile from "@/assets/images/logo.png" // Mobile logo
 import { cn } from '@/lib/utils'
 
 const Logo = ({ className }) => {
@@ -9,12 +10,19 @@ const Logo = ({ className }) => {
       to="/" 
       className={cn("flex items-center gap-1 shrink-0", className)}
     >
+      {/* Mobile Logo */}
+      <img 
+        src={logomobile} 
+        alt="Otomatiks Logo Mobile" 
+        className="block md:hidden h-14 w-44 object-contain"
+      />
+
+      {/* Desktop Logo */}
       <img 
         src={logo} 
-        alt="Otomatiks Logo" 
-        className="h-24 w-auto object-contain" // Increased height from h-10 to h-14
+        alt="Otomatiks Logo Desktop" 
+        className="hidden md:block h-24 w-auto object-contain"
       />
-      {/* <span className="font-bold text-3xl">Otomatiks</span> */}
     </Link>
   )
 }
